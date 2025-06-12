fe2aee68dd03b4165f6a6928054c998ade11cd14
dagbotho <thomas.dagbo@dagbocorp.com>
2025-06-11 22:10:08 -0400

    cee_segc

--- orders/views.py
+++ orders/views.py
@@ -1,3 +1,28 @@
-from django.shortcuts import render
+from cart.cart import Cart
+from django.shortcuts import redirect, render
 
-# Create your views here.
+from .forms import OrderCreateForm
+from .models import OrderItem
+from .tasks import order_created
+
+def order_create(request):
+    cart = Cart(request)
+    if request.method == 'POST':
+        form = OrderCreateForm(request.POST)
+        if form.is_valid():
+            order = form.save()
+            for item in cart:
+                OrderItem.objects.create(order=order,
+                                         product=item['product'],
+                                         price=item['price'],
+                                         quantity=item['quantity'])
+            # Clear the cart
+            cart.clear()
+            # Launch asynchronous task
+            order_created.delay(order.id)
+            request.session['order_id'] = order.id
+            return redirect('payment:process')
+           
+    else:
+        form = OrderCreateForm()
+    return render(request, 'orders/order/create.html', {'cart': cart, 'form': form})
